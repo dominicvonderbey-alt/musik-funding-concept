3abe16a742446946277d71e58fc04d024010b62d
dominicvonderbey-alt <dominicvonderbey@gmail.com>
2026-04-22 23:45:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker, Session, relationship
 import shutil
 import os
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = "sqlite:////tmp/users.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
